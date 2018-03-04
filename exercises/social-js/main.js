c73d1d585471e662169af2b2dc9bac8e20e77e59
laneykawa <lkmariko@gmail.com>
2018-03-04 01:45:30 -0700

    added color-gri, newsies, and social-js projects

--- exercises/social-js/main.js
+++ exercises/social-js/main.js
@@ -1,23 +1,53 @@
-var localDjs = {  
-    name: "Travis",
-    gender
-    age: 31,
-    height:
-    favoriteClimbs: [],
-    mountainClimbingFriends: [
-        {
-            name: "Sarah",
-            age: 31,
-            favoriteClimbs: [
-                {
-                    climbName: "Mount Everest",
-                    difficulty: 10
-                },
-//                ... etc ...
-            ]
+var localDjs = [
+    {
+        name: "Travis",
+        lastName: "Hayes",
+        local.Djs.fullName() {return this.name + " " + this.lastName;
+        djName: "Morgan Hayes",
+        gender: "male",
+        age: 31,
+        equipment: "cdjs",
+        favoriteGenreToDj: "deep house",
+        productionSoftareUse: "Ableton",
+        favoriteArtists: ["aly & fila", "tchami"],
+        otherDjFriends: [
+            {
+                name: "Dan",
+                lastName: "Hooten",
+                djName: "DJLvaLva",
+                gender: "male",
+                age: 31,
+                equipment: "cdjs",
+                favoriteGenreToDj: [
+                    {
+                        genreName: "Hip-Hop",
+                        subGenres: ["west-coast style", "memphis-style"],
+                    },
+                    productionSoftareUse: "Ableton Push",
+                    favoriteArtist: ""
         },
-        {
-
-        }
-    ]
-}
+            {
+                name: "Angelo",
+                lastName: "Dalisay",
+                djName: "ADAP",
+                gender: "male",
+                age: 33,
+                equipment: "cdj-sx",
+                favoriteGenreToDj: [
+                    {
+                        genreName: "house",
+                        subGenres: ["deep house", "electro house", "future house"],
+                    },
+                    {
+                        genreName: "hip-hop",
+                        subGenres: ["west-coast style", "memphis-style"],
+                    },
+                    {
+                        genreName: "country",
+                        subGenres: "country-swing",
+                    }
+                    productionSoftareUse: "Ableton",
+                    favoriteArtists: ["aly & fila", "tchami"],
+         }
+    }
+]
