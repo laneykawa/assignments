b4122a6ca0f599612e76fe38408accc5a01a7a46
laneykawa <lkmariko@gmail.com>
2018-03-05 14:20:57 -0700

    removed file from planet project

--- exercises/social-js/main.js
+++ exercises/social-js/main.js
@@ -1,53 +1,65 @@
 var localDjs = [
     {
-        name: "Travis",
+        firstName: "Travis",
         lastName: "Hayes",
-        local.Djs.fullName() {return this.name + " " + this.lastName;
         djName: "Morgan Hayes",
         gender: "male",
-        age: 31,
-        equipment: "cdjs",
-        favoriteGenreToDj: "deep house",
-        productionSoftareUse: "Ableton",
-        favoriteArtists: ["aly & fila", "tchami"],
-        otherDjFriends: [
-            {
-                name: "Dan",
-                lastName: "Hooten",
-                djName: "DJLvaLva",
-                gender: "male",
-                age: 31,
-                equipment: "cdjs",
-                favoriteGenreToDj: [
-                    {
-                        genreName: "Hip-Hop",
-                        subGenres: ["west-coast style", "memphis-style"],
-                    },
-                    productionSoftareUse: "Ableton Push",
-                    favoriteArtist: ""
+        age: 31, 
+        genreLikesToDj: "house",
+        subGenreLikesToDj: [ "deep-house"],
+        equipment: "Pioneer cdjs",
+        infoAboutDj: function (){
+            console.log("I am a " + this.genreLikesToDj + " DJ");
         },
+        favoriteArtists: [
             {
-                name: "Angelo",
-                lastName: "Dalisay",
-                djName: "ADAP",
-                gender: "male",
-                age: 33,
-                equipment: "cdj-sx",
-                favoriteGenreToDj: [
+                name: "Aly and Fila",
+                genre: "deep-house",
+                festivalsDjAt: [
                     {
-                        genreName: "house",
-                        subGenres: ["deep house", "electro house", "future house"],
+                        festivalName: "EDC", 
+                        location: "Las-Vegas",
                     },
                     {
-                        genreName: "hip-hop",
-                        subGenres: ["west-coast style", "memphis-style"],
-                    },
-                    {
-                        genreName: "country",
-                        subGenres: "country-swing",
+                        festivalName: "EDC",
+                        location: "California",
                     }
-                    productionSoftareUse: "Ableton",
-                    favoriteArtists: ["aly & fila", "tchami"],
-         }
+                ]
+            },
+            {
+                name: "Tchami",
+                genre: "deep-house",
+            },
+        ],
+        residentDjAt: ["texas-club", "Sky"],
+        djSoftware: "traktor",  
+    },
+    {
+        firstName: "Angelo",
+        lastName: "Dalisy",
+        djName: "ADAP",
+        gender: "male",
+        age: 32, 
+        genreLikesToDj: ["house", "edm", "90's Hip-Hop"],
+        subGenreLikesToDj: ["electro-house", "future house", "funk house"],
+        equipment: "Pioneer sx",
+        favoriteArtists: ["Tchami", "Porter Robinson", "Above and Beyond"],
+        residentDjAt: ["Huka Bar", "Sky"],
+        djSoftware: "serato",
+    },
+    {
+        firstName: "Dan",
+        lastName: "Hooten",
+        djName: "DJ LVA LVA",
+        gender: "male",
+        age: 35, 
+        genreLikesToDj: "hip-hop",
+        subGenreLikesToDj: [ "90's hip-hop", "dirty-south", "west-coast style"],
+        equipment: "Pioneer cdjs",
+        favoriteArtists: ["Aly and Fila", "Tchami"],
+        residentDjAt: ["texas-club", "Sky"],
+        djSoftware: "traktor",
     }
-]
+];
+
+localDjs[0].infoAboutDj();
--- exercises/strunctions/main.js
+++ exercises/strunctions/main.js
@@ -1,4 +1,75 @@
-var stringFunction = function (str){
-    
+// .concat()
 
+var stringFunction = function (str1, str2){
+    console.log(str1.concat(str2))
 }
+stringFunction("hello ", "my name is Laney");
+
+
+// .indexOf()
+var indexFunction = function(str){
+    console.log(str.indexOf("o"));
+}
+indexFunction("how are you today"); 
+
+
+// .lastIndexOf()
+var lastIndexFunction = function(str){
+    console.log(str.lastIndexOf("o"));
+}
+lastIndexFunction("how are you today"); 
+
+
+// .match()
+var matchFunction = function(str){
+    console.log(str.match("you"));
+}
+matchFunction("how are you today"); 
+
+
+// .replace()
+var replaceFunction = function (str){
+    console.log(str.replace(str, "just replaced"));
+}
+replaceFunction ("how are you today");
+
+
+// .slice()
+var sliceFunction = function (str){
+    console.log(str.slice(5,10));
+}
+sliceFunction ("how are you today");
+
+
+// .split()
+var splitFunction = function (str){
+    console.log(str.split(" "));
+}
+splitFunction ("how are you today");
+
+
+// .toLowerCase()
+var toLowerCaseFunction = function (str){
+    console.log(str.toLowerCase());
+}
+toLowerCaseFunction("How are you today");
+
+
+// .toUpperCase()
+var toUpperCaseFunction = function (str){
+    console.log(str.toUpperCase());
+}
+toUpperCaseFunction("How are you today");
+
+
+// .substr()
+var substrFunction = function (str){
+    console.log(str.substr(5,2));
+}
+substrFunction("How are you today");
+
+var testBoth = function(str){
+    console.log (str.toUpperCase .substr(2,2))
+}
+
+testBoth("how are you today?")
